c327b59ee964952dc831f72bfc4bb39afa9341a8
Devin Miller <email": null>
2017-11-13 14:04:29 -0800

    Create stylesheets folder and move stylesheets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './stylesheets/App.css';
 import Navbar from './components/Navbar';
 import Content from './components/Content';
 
