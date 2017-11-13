c180271a9c797abfb1f25ff4df55d6680b1f84e0
Devin Miller <email": null>
2017-11-13 12:27:24 -0800

    Create Navbar component and SCSS file for it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>Devin Miller</h1>
+          <Navbar />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../Navbar.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navbar;
