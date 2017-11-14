8fb447f3e453c61f4fd7eea99d336700d89515e5
Devin Miller <email": null>
2017-11-13 16:41:32 -0800

    Add content to the Home component and some styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,31 @@ class Home extends Component {
     return (
       <section className="home">
         <h2>Web Developer | Manager | Book Nerd</h2>
-
+        <p>
+          I'm Devin Miller, a web developer located in Seattle, WA. I'm a writer, whether it's poetry or documentation. I'm a history and comic book geek. My favorite rock in the world is on Vashon Island and my second favorite rock is the giant one next to the foot-bridge in Ravenna Park.
+        </p>
+        <div id="card-container">
+          <div id="technical-list" className="list-card">
+            <h3>What I know how to do...</h3>
+            <ul>
+              <li>Languages: Ruby, JavaScript, Python, HTML, CSS, SQL</li>
+              <li>Frameworks & Libraries: Ruby on Rails, Sinatra, JQuery, AJAX, ReactJS</li>
+              <li>Databases: PostgreSQL, SQLite3</li>
+              <li>Workflows: Agile development, pair programming, constructive feedback</li>
+              <li>Git, GitHub, Heroku, CircleCI</li>
+            </ul>
+          </div>
+          <div id="personal-list" className="list-card">
+            <h3>What I love to talk about...</h3>
+            <ul>
+              <li>Women's work during World War II</li>
+              <li>Diversity, inclusion, bias, microaggressions in the tech industry</li>
+              <li>Why you should be buying the book, dress, or record player you want from Goodwill on eBay</li>
+              <li>The Unbeatable Squirrel Girl</li>
+              <li>My cat, Oolong Kittea</li>
+            </ul>
+          </div>
+        </div>
       </section>
     )
   }
