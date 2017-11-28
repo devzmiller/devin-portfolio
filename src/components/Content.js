3e5dd74d48449accf62578d6cb0b4db7d7643e46
Devin Miller <devzmiller@gmail.com>
2017-11-27 18:14:26 -0800

    Add fade-in transitions. Doesn't work yet. Need to set in to false when you change content components.

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,26 +1,54 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition'
 import About from './About'
 import Projects from './Projects'
 import Home from './Home'
 
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered:  { opacity: 1 },
+};
+
+const Fade = ({ in: inProp, component }) => (
+  <Transition in={inProp} timeout={duration}>
+    {(state) => (
+      <div style={{
+        ...defaultStyle,
+        ...transitionStyles[state]
+      }}>
+      {component}
+      </div>
+    )}
+  </Transition>
+);
+
 class Content extends Component {
+
+
   render() {
     if (this.props.view === "about") {
       return (
         <main>
-          <About />
+          <Fade in={true} component={<About />} />
         </main>
       )
     } else if (this.props.view === "projects") {
       return (
         <main>
-          <Projects />
+          <Fade in={true} component={<Projects />} />
         </main>
       )
     } else if (this.props.view === "home") {
       return (
         <main>
-          <Home />
+          <Fade in={true} component={<Home />} />
         </main>
       )
     }
