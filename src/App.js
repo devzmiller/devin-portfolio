ceea0bae1072ac8fea6027b6373689a93a676776
Devin Miller <devzmiller@gmail.com>
2018-01-02 16:27:22 -0800

    Finish adding social media logo links

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './stylesheets/App.css';
 import Navbar from './components/Navbar';
 import Content from './components/Content';
-import githubLogo from './images/GitHub-Mark-64px.png'
+import githubLogo from './images/GitHub-Mark-32px.png';
+import linkedinLogo from './images/In-Black-34px-TM.png';
+import emailLogo from './images/2000px-Aiga_mail.svg.png';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
@@ -24,7 +26,9 @@ class App extends Component {
           <h1 id="home" onClick={(event) => this.go(event)}>Devin Miller</h1>
           <h2>Software Developer | Manager | Book Nerd</h2>
           <div className="social-logos">
-            <img src={githubLogo} className="social-logo" alt="my github"/>
+            <a href="mailto:devzmiller@gmail.com"><img src={emailLogo} className="social-logo" alt="my email"/></a>
+            <a href="https://github.com/devzmiller" target="_blank"><img src={githubLogo} className="social-logo" alt="my github"/></a>
+            <a href="https://www.linkedin.com/in/devzmiller/" target="_blank"><img src={linkedinLogo} className="social-logo" alt="my linkedin"/></a>
           </div>
           <Navbar onClick={(event) => this.go(event)}/>
         </header>
