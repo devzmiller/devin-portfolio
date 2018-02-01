70742704c5facdab3a2e6fa177889a1c17443cb0
Devin Miller <devzmiller@gmail.com>
2018-01-31 17:26:45 -0800

    Make about and contact pages more mobile friendly

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,23 +8,25 @@ import resume from '../DevinMiller-Resume.pdf';
 class Contact extends Component {
   render() {
     return(
-      <div className="social-logos">
-        <div className="social-logo">
-          <a href="mailto:devzmiller@gmail.com"><img src={emailLogo} alt="my email"/></a>
-          <p>Send me an email.</p>
-        </div>
-        <div className="social-logo">
-          <a href="https://github.com/devzmiller" target="_blank"><img src={githubLogo} alt="my github"/></a>
-          <p>Explore my code on GitHub.</p>
-        </div>
-        <div className="social-logo">
-          <a href="https://www.linkedin.com/in/devzmiller/" target="_blank"><img src={linkedinLogo} alt="my linkedin"/></a>
-          <p>Connect with me on LinkedIn.</p>
+      <section className="contact-buttons">
+        <div className="social-logos">
+          <div className="social-logo">
+            <a href="mailto:devzmiller@gmail.com"><img src={emailLogo} alt="my email"/></a>
+            <p>Send me an email.</p>
+          </div>
+          <div className="social-logo">
+            <a href="https://github.com/devzmiller" target="_blank"><img src={githubLogo} alt="my github"/></a>
+            <p>Explore my code on GitHub.</p>
+          </div>
+          <div className="social-logo">
+            <a href="https://www.linkedin.com/in/devzmiller/" target="_blank"><img src={linkedinLogo} alt="my linkedin"/></a>
+            <p>Connect with me on LinkedIn.</p>
+          </div>
         </div>
         <div className="resume-button">
           <a href={resume} target="_blank">Download My Resume</a>
         </div>
-      </div>
+      </section>
     )
   }
 }
